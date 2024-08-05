36a44d361f4b64b7d3aff091ef390d298feb0e39
JustAnAverageMax <theaveragemaximum@gmail.com>
2024-08-05 14:50:26 +0300

    Implement CustomArrayList

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,28 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import Lists.CustomArrayList;
+
+import java.util.HashSet;
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        CustomArrayList<Integer> test = new CustomArrayList<>();
+        System.out.println("Default capacity: " + test.getCapacity());
+        int removeIndex = 0;
+        int addIndex = 4;
+        test.add(1);
+        test.add(2);
+        test.add(3);
+        test.add(4);
+        test.add(5);
+        test.add(6);
+        test.add(7);
+        test.add(8);
+        test.add(9);
+        System.out.println("Current capacity: " + test.getCapacity());
+        System.out.println(test);
+        int removedValue = test.remove(0);
+        System.out.println("Removed element at index " + removeIndex + ": " + removedValue + "\n" + test);
+        test.add(addIndex, removedValue);
+        System.out.println("Added removed value (" + removedValue + ") at index " + addIndex + "\n" + test);
     }
 }

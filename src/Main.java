2011084618434c4dfdec082afa4adc35ff6d804c
JustAnAverageMax <theaveragemaximum@gmail.com>
2024-08-05 17:22:05 +0300

    Implement CustomHashSet

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,56 @@
 import Lists.CustomArrayList;
+import Sets.CustomHashSet;
 
 import java.util.HashSet;
 
 
 public class Main {
     public static void main(String[] args) {
-        CustomArrayList<Integer> test = new CustomArrayList<>();
-        System.out.println("Default capacity: " + test.getCapacity());
+        CustomArrayList<Integer> arrayListTest = new CustomArrayList<>();
+        System.out.println("Default capacity: " + arrayListTest.getCapacity());
         int removeIndex = 0;
         int addIndex = 4;
-        test.add(1);
-        test.add(2);
-        test.add(3);
-        test.add(4);
-        test.add(5);
-        test.add(6);
-        test.add(7);
-        test.add(8);
-        test.add(9);
-        System.out.println("Current capacity: " + test.getCapacity());
-        System.out.println(test);
-        int removedValue = test.remove(0);
-        System.out.println("Removed element at index " + removeIndex + ": " + removedValue + "\n" + test);
-        test.add(addIndex, removedValue);
-        System.out.println("Added removed value (" + removedValue + ") at index " + addIndex + "\n" + test);
+        arrayListTest.add(1);
+        arrayListTest.add(2);
+        arrayListTest.add(3);
+        arrayListTest.add(4);
+        arrayListTest.add(5);
+        arrayListTest.add(6);
+        arrayListTest.add(7);
+        arrayListTest.add(8);
+        arrayListTest.add(9);
+        System.out.println("Current capacity: " + arrayListTest.getCapacity());
+        System.out.println(arrayListTest);
+        int removedValue = arrayListTest.remove(0);
+        System.out.println("Removed element at index " + removeIndex + ": " + removedValue + "\n" + arrayListTest);
+        arrayListTest.add(addIndex, removedValue);
+        System.out.println("Added removed value (" + removedValue + ") at index " + addIndex + "\n" + arrayListTest);
+
+        CustomHashSet<String> hashTest = new CustomHashSet<>();
+        hashTest.put("Java");
+        hashTest.put("C++");
+        hashTest.put("Python");
+        hashTest.put("Ruby");
+        hashTest.put("Assembler");
+        hashTest.put("C#");
+        hashTest.put("C");
+        hashTest.put("php");
+        hashTest.put("Pascal");
+        hashTest.put("Fortran");
+        hashTest.put("Delphi");
+        hashTest.put("HolyC");
+        hashTest.put("Haskell");
+        System.out.println("Set size: " + hashTest.size());
+        for (String s: hashTest){
+            System.out.print(s + " ");
+        }
+        System.out.println("\nAttempt to add Java: " + hashTest.put("Java"));
+        System.out.println("Set contains string \"C++\": " + hashTest.contains("C++"));
+        System.out.println("Attempt to remove something that is not in the set: " + hashTest.delete("Capybara"));
+        System.out.println("Removing string \"C++\": " + hashTest.delete("C++"));
+        System.out.println("Set size: " + hashTest.size());
+        for (String s: hashTest){
+            System.out.print(s + " ");
+        }
     }
 }
--- src/Sets/CustomHashSet.java
+++ src/Sets/CustomHashSet.java
@@ -1,2 +1,141 @@
-package Sets;public class CustomHashSet {
+package Sets;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class CustomHashSet<T> implements Iterable<T> {
+    private static final float LOAD_FACTOR = 0.75f;
+    private static final int DEFAULT_CAPACITY = 16;
+
+    private Object[] data;
+    private int size;
+
+    private class CustomIterator implements Iterator<T> {
+
+        private int currentIndex = 0;
+        private int elementsVisited = 0;
+
+        @Override
+        public boolean hasNext() {
+            return this.elementsVisited < size;
+        }
+
+        @Override
+        @SuppressWarnings("unchecked")
+        public T next() {
+            if (!hasNext())
+                throw new NoSuchElementException();
+            while (currentIndex < data.length && data[currentIndex] == null)
+                currentIndex++;
+            if(currentIndex >= data.length){
+                throw new NoSuchElementException();
+            }
+            elementsVisited++;
+            return (T) data[currentIndex++];
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private void ensureCapacity() {
+        if (this.size >= this.data.length * LOAD_FACTOR) {
+            System.out.println("Set " + this + " just got resized");
+            int newSize = this.data.length * 2;
+            Object[] newData = new Object[newSize];
+            for (Object element : this.data) {
+                if (element != null) {
+                    int index = getIndex((T) element, newSize);
+                    while (newData[index] != null) {
+                        index = (index + 1) % newSize;
+                    }
+                    newData[index] = element;
+                }
+            }
+            this.data = newData;
+        }
+    }
+
+    private int getIndex(T element) {
+        return getIndex(element, this.data.length);
+    }
+
+    private int getIndex(T element, int length) {
+        return Math.abs(element.hashCode() % length);
+    }
+
+    public CustomHashSet() {
+        this.data = new Object[DEFAULT_CAPACITY];
+        this.size = 0;
+    }
+
+    public boolean put(T element) {
+        ensureCapacity();
+        int index = getIndex(element);
+        if (this.data[index] == null) {
+            this.data[index] = element;
+            this.size++;
+            return true;
+        } else if (data[index].equals(element)) {
+            return false;
+        } else {
+            for (int i = index + 1; i != index; i = (i + 1) % this.data.length) {
+                if (this.data[i] == null) {
+                    this.data[i] = element;
+                    this.size++;
+                    return true;
+                } else if (this.data[i].equals(element)) {
+                    return false;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean contains(T element) {
+        int index = getIndex(element);
+        if (this.data[index] == null) {
+            return false;
+        } else if (this.data[index].equals(element)) {
+            return true;
+        } else {
+            for (int i = index + 1; i != index; i = (i + 1) % this.data.length) {
+                if (this.data[i] == null) {
+                    return false;
+                } else if (this.data[i].equals(element)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean delete(T element) {
+        int index = getIndex(element);
+        if (this.data[index] == null) {
+            return false;
+        } else if (this.data[index].equals(element)) {
+            this.data[index] = null;
+            this.size--;
+            return true;
+        } else {
+            for (int i = index + 1; i != index; i = (i + 1) % this.data.length) {
+                if (this.data[i] == null) {
+                    return false;
+                } else if (this.data[i].equals(element)) {
+                    this.data[i] = null;
+                    this.size--;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new CustomIterator();
+    }
 }
